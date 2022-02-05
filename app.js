5a6f8a62d36c1af34631f25eb681cd2687109fcc
tudao <nevarda9416@gmail.com>
2022-02-05 21:59:13 +0700

    - [KoaJS]: koa-router koa-ejs axios koa-logger

--- app.js
+++ app.js
@@ -1,39 +1,30 @@
-var koa = require('koa');
-var app = new koa();
-// This function is a middleware, which gets called whenever our server gets a request
-// The context of this generator is called context in Koa
-// This context is used to access and modify the request and response objects
-/**
- * Generators
- * Used a function*() notation to pass a callback to app.use()
- * Koa is an object, which contains an array of middleware generator functions, all of which are composed and executed in a stack-like manner upon each request
- * Koa also implements downstreaming followed by upstreaming of control flow
- */
-app.use(function*(next){
-    // do something before yielding to next generator function
-    // in line which will be 1st event in downstream
-    console.log("1");
-    yield next;
-    // do something when the execution returns upstream, this will be last event in upstream
-    console.log("2");
+const koa = require("koa");
+const path = require("path");
+const render = require("koa-ejs");
+const koaRouter = require("koa-router");
+const axios = require("axios");
+const Logger = require("koa-logger");
+// Initialize
+const app = new koa();
+const router = new koaRouter();
+
+render(app, {
+    root: path.join(__dirname, "views"),
+    layout: "index",
+    viewExt: "html",
 });
-app.use(function*(next){
-    // This shall be 2nd event downstream
-    console.log("3");
-    yield next;
-    // This would be 2nd event upstream
-    console.log("4");
+
+router.get("hello", "/", (ctx) => {
+    ctx.body = "<h1>Hello World, Koa folks!</h1>";
 });
-app.use(function*(){
-    // Here it would be last function downstream
-    console.log("5");
-    // Set response body
-    this.body = "Hello Generators";
-    // First event of upstream (from the last to first)
-    console.log("6");
+
+router.get("users", "/users", async (ctx) => {
+    const result = await axios.get("https://randomuser.me/api?results=5");
+    return ctx.render("index", {
+        users: result.data.results
+    });
 });
-// This function binds and listens for connections on the specified port
-// Port is the only required parameter here. The callback function is executed, if the app runs successfully
-app.listen(3000, function(){
-    console.log('Server running on http://localhost:3000')
-});
+
+app.use(Logger()).use(router.routes()).use(router.allowedMethods());
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`running on port ${PORT}`));
